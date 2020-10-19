e48dad7c37b3ffb9c623606d4e1cb4909e1dc311
happiguru <hguruman@gmail.com>
2020-10-19 21:46:04 +0400

    ruby solution

--- repeated_string.rb
+++ repeated_string.rb
@@ -1,20 +1,13 @@
-input = 'abcdef'
- 
-chars = input.split('')
-puts chars.length
-puts chars[2]
-
- 
- 
-# chars.each { |c|
-#     i= 0
-#     print c << chars[i]
-#     i+=1
-# }
-
-n = 0
-while(n<10)
-    i= 0
-    print chars << chars[i]
-    i+=1
+def repeatedString(s, n)
+    repeat = 0
+    strlenth = s.length
+    toarray = s.split('')
+    num_repeat = (n / strlenth).floor()
+    num_repeat = num_repeat * s.count('a')
+    for i in 0 ... n % strlenth
+        if (toarray[i]) == 'a'
+            repeat = repeat + 1
+        end
+    end
+    return repeat = num_repeat + repeat
 end

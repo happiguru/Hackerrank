57ec42917addc919f0c3564080909fcea4187a1e
happiguru <hguruman@gmail.com>
2021-07-15 13:46:23 +0400

    Refactor code

--- RUBY/toh.rb
+++ RUBY/toh.rb
@@ -3,24 +3,37 @@ def move(starting, goal)
   
   if starting == 1
     mid = 2
-    toh(n, starting, mid, goal)
+    toh(n, starting, goal, mid)
   else
     mid = 1
-    toh(n, starting, mid, goal)
+    toh(n, starting, goal, mid)
   end
 
 end
 
-def toh(n, starting, mid, goal)
+# def toh(n, starting, mid, goal)
+#   if n == 1
+#     puts "#{starting} -> #{goal}"
+#     return
+#   end
+#   toh(n-1, starting, goal, mid)
+#   puts "#{starting} -> #{goal}"
+#   toh(n-1, mid, starting, goal)
+#   nil
+# end
+
+def toh(n, starting, goal, mid)
   if n == 1
     puts "#{starting} -> #{goal}"
-    return
+  else
+    toh(n-1, starting, mid, goal)
+    # toh(1, starting, goal, mid)
+    puts "#{starting} -> #{goal}"
+    toh(n-1, mid, goal, starting)
+    
   end
-  toh(n-1, starting, goal, mid)
-  puts "#{starting} -> #{goal}"
-  toh(n-1, mid, starting, goal)
-  nil
 end
+
 
 puts move(1, 3)
 
